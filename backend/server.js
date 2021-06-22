fec685ac1a6e28c0d9cbcc438cf965f74eaa8d07
Aosenba <aosenbalongchar@gmail.com>
2021-06-22 12:45:30 +0530

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ import path from 'path';
 
 dotenv.config();
 
-const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
@@ -20,9 +19,14 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/vicarious',{
     useCreateIndex : true   
 });
 
+
+
+
+
+
+
 app.use('/api/uploads',uploadRouter);
-app.use(express.static(path.join(__dirname,'/frontend/build')));
-app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'/frontend/build/index.html')));
+
 app.use('/api/users', userRouter); 
 
 app.use('/api/products',productRouter);
@@ -39,9 +43,12 @@ app.use((err,req,res,next)=>
     res.status(500).send({message : err.message});
 });
 
+const __dirname = path.resolve();
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname,'/frontend/build')));
+app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'/frontend/build/index.html')));
 app.listen(port, () =>
 {
     console.log(`server is at http://localhost:${port}`);
