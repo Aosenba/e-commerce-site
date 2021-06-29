a6c5380a953c62edfc948cfa8a64cfb16c4b9643
Aosenba <aosenbalongchar@gmail.com>
2021-06-29 15:36:07 +0530

    publish to heroku

--- backend/config.js
+++ backend/config.js
@@ -7,7 +7,6 @@ export default {
   PORT: process.env.PORT || 5000,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
   secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
 };
